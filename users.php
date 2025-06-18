86ff595196f8d88a479dfff3a1f9bf75413db8b6
ld-web <lucas@ld-web.net>
2025-06-18 10:33:39 +0200

    :tada: index, users list (JSON)

---
+++ users.php
@@ -0,0 +1,142 @@
+<?php
+
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Roberts',
+        'firstname' => 'Jennie',
+        'email' => 'ila@hocretuv.bw'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Perry',
+        'firstname' => 'Fanny',
+        'email' => 'pe@kojib.sy'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Hubbard',
+        'firstname' => 'Bessie',
+        'email' => 'puko@aniuz.et'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Romero',
+        'firstname' => 'Callie',
+        'email' => 'cegaburug@kefzaden.co'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Guerrero',
+        'firstname' => 'Antonio',
+        'email' => 'meolodi@okarub.ec'
+    ],
+    [
+        'id' => 6,
+        'name' => 'Franklin',
+        'firstname' => 'Ruth',
+        'email' => 'zecjucuho@kalipku.je'
+    ],
+    [
+        'id' => 7,
+        'name' => 'Harmon',
+        'firstname' => 'Jayden',
+        'email' => 'ufrez@hidifoge.io'
+    ],
+    [
+        'id' => 8,
+        'name' => 'Poole',
+        'firstname' => 'Lulu',
+        'email' => 'wojju@sohacked.hk'
+    ],
+    [
+        'id' => 9,
+        'name' => 'Palmer',
+        'firstname' => 'Jay',
+        'email' => 'divefumi@bahjuj.za'
+    ],
+    [
+        'id' => 10,
+        'name' => 'Diaz',
+        'firstname' => 'Sue',
+        'email' => 'civ@ag.gf'
+    ],
+    [
+        'id' => 11,
+        'name' => 'Jenkins',
+        'firstname' => 'Manuel',
+        'email' => 'ecejo@ziicu.vg'
+    ],
+    [
+        'id' => 12,
+        'name' => 'Berry',
+        'firstname' => 'Pearl',
+        'email' => 'od@nomaci.tc'
+    ],
+    [
+        'id' => 13,
+        'name' => 'Ramirez',
+        'firstname' => 'Millie',
+        'email' => 'coltilwar@hecfumle.ar'
+    ],
+    [
+        'id' => 14,
+        'name' => 'Munoz',
+        'firstname' => 'Nina',
+        'email' => 'gimowvor@kov.cr'
+    ],
+    [
+        'id' => 15,
+        'name' => 'Torres',
+        'firstname' => 'Paul',
+        'email' => 'noz@nimehlic.cc'
+    ],
+    [
+        'id' => 16,
+        'name' => 'Goodman',
+        'firstname' => 'Joseph',
+        'email' => 'rofra@cihemif.ky'
+    ],
+    [
+        'id' => 17,
+        'name' => 'Elliott',
+        'firstname' => 'Bess',
+        'email' => 'nazihnac@wezav.sd'
+    ],
+    [
+        'id' => 18,
+        'name' => 'Todd',
+        'firstname' => 'Julia',
+        'email' => 'ne@cona.za'
+    ],
+    [
+        'id' => 19,
+        'name' => 'Gomez',
+        'firstname' => 'Susie',
+        'email' => 'vabbe@cirout.gw'
+    ],
+    [
+        'id' => 20,
+        'name' => 'Evans',
+        'firstname' => 'Alice',
+        'email' => 'hav@nocnep.ls'
+    ],
+    [
+        'id' => 21,
+        'name' => 'Valdez',
+        'firstname' => 'Cora',
+        'email' => 'ju@hagne.lb'
+    ],
+    [
+        'id' => 22,
+        'name' => 'Simon',
+        'firstname' => 'Edna',
+        'email' => 'gagsevhof@jahumul.ar'
+    ],
+    [
+        'id' => 23,
+        'name' => 'Flowers',
+        'firstname' => 'Stella',
+        'email' => 'roswa@beeveuc.aw'
+    ],
+];
